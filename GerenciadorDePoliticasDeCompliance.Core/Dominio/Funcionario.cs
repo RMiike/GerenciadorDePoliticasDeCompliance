a9220c540bc7e0aa9ac134b741b848d4c7488706
Renato Miike <miike_miike@hotmail.com>
2020-01-16 20:51:32 -0300

    Adicionada F

--- GerenciadorDePoliticasDeCompliance.Core/Dominio/Funcionario.cs
+++ GerenciadorDePoliticasDeCompliance.Core/Dominio/Funcionario.cs
@@ -1,9 +1,17 @@
 ﻿namespace GerenciadorDePoliticasDeCompliance.Core.Dominio
 {
-    class Funcionario
+    public class Funcionario
     {
+        public Funcionario(string nome, int cpf, int matricula, string email)
+        {
+            Nome = nome;
+            CPF = cpf;
+            Matricula = matricula;
+            Email = email;
+        }
+
         public int Id { get; set; }
-        
+
         public string Nome { get; set; }
 
         public int CPF { get; set; }
@@ -12,6 +20,10 @@
         
         public string Email { get; set; }
 
-        public string Senha { get; set; }
+        public Usuario Usuario { get; set; }
+
+
+
+
     }
 }
--- GerenciadorDePoliticasDeCompliance.Web/Controllers/FuncionariosController.cs
+++ GerenciadorDePoliticasDeCompliance.Web/Controllers/FuncionariosController.cs
@@ -1,6 +1,8 @@
 ﻿using GerenciadorDePoliticasDeCompliance.Core.BancoDeDados;
+using GerenciadorDePoliticasDeCompliance.Core.Dominio;
 using GerenciadorDePoliticasDeCompliance.Models.Funcionarios;
 using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
 
 namespace GerenciadorDePoliticasDeCompliance.Controllers
 {
@@ -8,6 +10,10 @@ namespace GerenciadorDePoliticasDeCompliance.Controllers
     {
         private Conexao Conexao { get; set; }
 
+        public FuncionariosController()
+        {
+            Conexao = new Conexao();
+        }
         public IActionResult Index()
         {
             return View();
@@ -18,11 +24,29 @@ namespace GerenciadorDePoliticasDeCompliance.Controllers
         }
 
         [HttpPost]
-        public void Cadastrar(FormularioViewModel modelo)
+        public IActionResult Cadastrar(FormularioViewModel modelo)
         {
-            Conexao.Conectar();
-            Conexao.ExecutarQuery("");
-            Conexao.Desconectar();
+            Funcionario funcionario = modelo.ConverterParaFuncionario();
+            Usuario usuario = modelo.ConverterParaUsuario();
+
+           
+
+            var comandosql = new SqlCommand(Queries.QUERY_CADASTRO_USUARIO);
+            comandosql.Parameters.AddWithValue("@Email", usuario.Email);
+            comandosql.Parameters.AddWithValue("@Senha", usuario.Senha);
+            comandosql.Parameters.AddWithValue("@IdPerfil", (int)usuario.Perfil);
+            int idusuario = Conexao.ExecutarQuery(comandosql);
+
+            comandosql = new SqlCommand(Queries.QUERY_CADASTRO_FUNCIONARIO);
+            comandosql.Parameters.AddWithValue("@Nome", funcionario.Nome);
+            comandosql.Parameters.AddWithValue("@CPF", funcionario.CPF);
+            comandosql.Parameters.AddWithValue("@Matricula", funcionario.Matricula);
+            comandosql.Parameters.AddWithValue("@Email", funcionario.Email);
+            comandosql.Parameters.AddWithValue("@IdUsuario", idusuario);
+            Conexao.ExecutarQuery(comandosql);
+
+
+            return RedirectToAction("Index");
         }
     }
 }
