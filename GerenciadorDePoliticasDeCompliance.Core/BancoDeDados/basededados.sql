464d462b04345e3bc9d2de3c86a7530569d40675
Renato Miike <miike_miike@hotmail.com>
2020-01-23 14:46:51 -0300

    banco de dados publico azure anexado

---
+++ GerenciadorDePoliticasDeCompliance.Core/BancoDeDados/basededados.sql
@@ -0,0 +1,92 @@
+﻿
+/****** Object:  Table [dbo].[AssinaturaPolitica]    Script Date: 23/01/2020 12:31:58 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[AssinaturaPolitica](
+	[IdFuncionario] [int] NOT NULL,
+	[IdPolitica] [int] NOT NULL,
+	[Data] [datetime] NOT NULL
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Funcionario]    Script Date: 23/01/2020 12:31:59 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Funcionario](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Nome] [varchar](50) NOT NULL,
+	[Cpf] [float] NOT NULL,
+	[Matricula] [float] NOT NULL,
+	[Email] [varchar](30) NOT NULL,
+	[IdUsuario] [int] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Perfil]    Script Date: 23/01/2020 12:31:59 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Perfil](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Descricao] [varchar](1000) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Politica]    Script Date: 23/01/2020 12:31:59 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Politica](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Titulo] [text] NOT NULL,
+	[Texto] [varchar](8000) NOT NULL,
+	[Data] [datetime] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Usuario]    Script Date: 23/01/2020 12:31:59 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Usuario](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Email] [varchar](30) NOT NULL,
+	[Senha] [varchar](512) NOT NULL,
+	[IdPerfil] [int] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[AssinaturaPolitica]  WITH NOCHECK ADD FOREIGN KEY([IdFuncionario])
+REFERENCES [dbo].[Funcionario] ([Id])
+GO
+ALTER TABLE [dbo].[AssinaturaPolitica]  WITH NOCHECK ADD FOREIGN KEY([IdPolitica])
+REFERENCES [dbo].[Politica] ([Id])
+GO
+ALTER TABLE [dbo].[Funcionario]  WITH NOCHECK ADD  CONSTRAINT [FK_Funcionario_Usuario] FOREIGN KEY([IdUsuario])
+REFERENCES [dbo].[Usuario] ([Id])
+GO
+ALTER TABLE [dbo].[Funcionario] NOCHECK CONSTRAINT [FK_Funcionario_Usuario]
+GO
+ALTER TABLE [dbo].[Usuario]  WITH NOCHECK ADD  CONSTRAINT [FK_Usuario_Perfil] FOREIGN KEY([IdPerfil])
+REFERENCES [dbo].[Perfil] ([Id])
+GO
+ALTER TABLE [dbo].[Usuario] NOCHECK CONSTRAINT [FK_Usuario_Perfil]
+GO
